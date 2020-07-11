c14e808455bf65b56212c361dec0371a0804500d
lesterpoli <51953349+lesterpoli@users.noreply.github.com>
2020-07-11 01:13:18 -0700

    Add files via upload

---
+++ Sign-up.php
@@ -0,0 +1,28 @@
+<?php include('server.php')?>
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<form method="POST" action="Sign-up.php">
+    <table>
+        <tr>
+            <td>NAME:</td>
+            <td><input type="text" name="name"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input type="text" name="email"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input type="password" name="password"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input type="password" name="password2"></td>
+        </tr>
+        <tr><button name="" type="submit">submit</button></tr>
+    </table>
+</form>
+</body>
+</html>
