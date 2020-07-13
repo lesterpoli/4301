7e80bf0f8a426cce2c01b243e14456a8bb29891f
lesterpoli <51953349+lesterpoli@users.noreply.github.com>
2020-07-13 07:47:36 -0700

    Add files via upload

---
+++ Hardware/Login/Server_side/server_homepage.js
@@ -0,0 +1,11 @@
+let tab = document.getElementById("tab");
+let val = document.getElementById("val").value;
+let val2 = document.getElementById("val2").value;
+function CreateTable(){
+    // for(i=0; i<5;i++){
+        let row = tab.insertRow();
+//         for(j = 0; j<5; j++) {
+//             var yow = row.insertCell();
+//         }
+//     }
+}
